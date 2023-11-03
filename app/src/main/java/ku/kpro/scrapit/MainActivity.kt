14403445074500e7785eb70e9f3f9d964c5a6385
Sang-Min Kang <rdrniel12345@gmail.com>
2023-11-03 19:14:36 +0900

    init project

---
+++ app/src/main/java/ku/kpro/scrapit/MainActivity.kt
@@ -0,0 +1,11 @@
+package ku.kpro.scrapit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
