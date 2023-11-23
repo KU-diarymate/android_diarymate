788b6361f5af2af37484bfa0e26e7ec7bfff60ea
cyLee1111 <flora021111@gmail.com>
2023-11-23 23:01:50 +0900

    feat: popup, fix: setting layout

--- app/src/main/java/ku/kpro/diary_mate/data/ChatMessage.kt
+++ app/src/main/java/ku/kpro/diary_mate/data/ChatMessage.kt
@@ -1,3 +1,6 @@
 package ku.kpro.diary_mate.data
 
-data class ChatMessage(val sender: String, val message: String)
+data class ChatMessage(
+    val sender: String,
+    val message: String
+)
--- app/src/main/java/ku/kpro/diary_mate/etc/DiaryMateApplication.kt
+++ app/src/main/java/ku/kpro/diary_mate/etc/DiaryMateApplication.kt
@@ -1,13 +1,23 @@
 package ku.kpro.diary_mate.etc
 
 import android.app.Application
+import com.renju_note.isoo.util.PreferenceUtil
 import io.realm.Realm
 import io.realm.RealmConfiguration
+import ku.kpro.diary_mate.data.DiaryMateSetting
 
 class DiaryMateApplication : Application() {
 
+    companion object {
+        val setting = DiaryMateSetting.getDefaultSetting()
+        lateinit var pref : PreferenceUtil
+    }
+
     override fun onCreate() {
         super.onCreate()
+
+        pref = PreferenceUtil(this)
+        setting.setSettingData(pref.getString("setting", ""))
 
         Realm.init(this)
         val config : RealmConfiguration = RealmConfiguration.Builder()
